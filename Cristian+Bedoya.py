9f702960fa6f4f2d8fd8f245c968ac2ee3313a57
cristian <cristianbedoya1123@hotmail.com>
2016-08-08 20:35:37 -0500

    actualizacion

---
+++ Cristian+Bedoya.py
@@ -0,0 +1,135 @@
+
+# coding: utf-8
+
+# In[3]:
+
+print "Hola Mundo"
+
+
+# $\sum_{p}li(x^p)-log(2)$
+
+# In[ ]:
+
+
+
+
+# # Hola mundo
+# python
+
+# In[9]:
+
+2+4+5/6.0-3
+
+
+# In[8]:
+
+3/2
+
+
+# In[10]:
+
+3.0/2
+
+
+# In[11]:
+
+3+5j
+
+
+# In[12]:
+
+(3+5j)-(1-1j)
+
+
+# In[13]:
+
+abs(3+5j)
+
+
+# In[14]:
+
+5**3
+
+
+# In[15]:
+
+int(_13)
+
+
+# In[16]:
+
+float(_15)
+
+
+# In[17]:
+
+round(_13)
+
+
+# In[18]:
+
+complex(_15)
+
+
+# In[19]:
+
+max(2,8,30)
+
+
+# In[20]:
+
+min(2,9,32)
+
+
+# In[21]:
+
+a=5
+
+
+# In[22]:
+
+print a
+
+
+# In[23]:
+
+a
+
+
+# In[24]:
+
+x,y,z,a=1,2,3,4
+
+
+# In[25]:
+
+x,y=a,z
+
+
+# In[26]:
+
+x
+
+
+# In[27]:
+
+y
+
+
+# In[28]:
+
+x,y=5,8
+
+
+# In[30]:
+
+if x>y:
+    print("el mayor es",y)
+else:
+    print("el menor es",x)
+
+
+# In[ ]:
+
+
+
